b28a48b558e3e10587a6d7e5230eb7060a168551
GustavoPBrum <brumtavo@gmail.com>
2025-05-30 10:30:04 +0100

    SellerForm

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -106,33 +111,33 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load(); // Carrega a view
-//
-//			SellerFormController controller = loader.getController(); // Controller da instancia atual
-//			controller.setSeller(obj);
-//			controller.setSellerService(new SellerService()); // Injetamos o service
-//
-//			// O proprio objeto se inscreve para receber o evento
-//			controller.subscribeDataChanceLinstener(this);
-//
-//			controller.updateFormData(); // Vai carregar os dados do Seller vazio nas caixas de texto txtId e
-//											// txtName
-//
-//			// Um palco na frente de outro
-//			Stage dialogStage = new Stage(); // Novo Stage
-//			dialogStage.setTitle("Enter Seller data");
-//			dialogStage.setScene(new Scene(pane)); // Nova scene
-//			dialogStage.setResizable(false); // Se pode ser redimensionada
-//			dialogStage.initOwner(parentStage); // Stage **pai** dessa janela
-//
-//			// Enquanto a janela nao for fechada, nao pode acessar a janela anterior
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//			dialogStage.showAndWait();
-//		} catch (IOException e) {
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load(); // Carrega a view
+
+			SellerFormController controller = loader.getController(); // Controller da instancia atual
+			controller.setSeller(obj);
+			controller.setSellerService(new SellerService()); // Injetamos o service
+
+			// O proprio objeto se inscreve para receber o evento
+			controller.subscribeDataChanceLinstener(this);
+
+			controller.updateFormData(); // Vai carregar os dados do Seller vazio nas caixas de texto txtId e
+											// txtName
+
+			// Um palco na frente de outro
+			Stage dialogStage = new Stage(); // Novo Stage
+			dialogStage.setTitle("Enter Seller data");
+			dialogStage.setScene(new Scene(pane)); // Nova scene
+			dialogStage.setResizable(false); // Se pode ser redimensionada
+			dialogStage.initOwner(parentStage); // Stage **pai** dessa janela
+
+			// Enquanto a janela nao for fechada, nao pode acessar a janela anterior
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
