7c87a4877dad7c6c9357a7dbb1953155f6b7e80b
GustavoPBrum <brumtavo@gmail.com>
2025-05-30 10:08:28 +0100

    SellerList

---
+++ src/gui/SellerListController.java
@@ -0,0 +1,185 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import application.Main;
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Seller;
+import model.services.SellerService;
+
+public class SellerListController implements Initializable, DataChangeListener {
+
+	private SellerService service;
+
+	@FXML
+	private TableView<Seller> tableViewSeller;
+
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnId;
+
+	@FXML
+	private TableColumn<Seller, String> tableColumnName;
+
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnEDIT;
+
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnREMOVE;
+
+	@FXML
+	private Button btNew;
+
+	private ObservableList<Seller> obsList;
+
+	@FXML
+	public void onBtNewAction(ActionEvent event) { // Para ter uma referencia para o controller que recebeu o event
+		Stage parentStage = Utils.currentStage(event); // referencia para o stage atual
+		Seller obj = new Seller(); // Seller vazio
+		createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
+	}
+
+	public void setSellerService(SellerService service) {
+		this.service = service; // Injecao de dependencia com principio SOLID
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+
+	}
+
+	private void initializeNodes() {
+		// Padrao JavaFX para iniciar o comportamento das colunas
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+		Stage stage = (Stage) Main.getMainScene().getWindow(); // Ref para janela principal
+		tableViewSeller.prefHeightProperty().bind(stage.heightProperty()); // Jeito de fazer o table Seller
+																				// acompanhar a altura da janela
+	}
+
+	// Acessa o service, carrega os Sellere e jogar no obsList
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Seller> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+
+		tableViewSeller.setItems(obsList);
+		initEditButtons();
+		initRemoveButtons();
+	}
+
+
+	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
+//		try {
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+//			Pane pane = loader.load(); // Carrega a view
+//
+//			SellerFormController controller = loader.getController(); // Controller da instancia atual
+//			controller.setSeller(obj);
+//			controller.setSellerService(new SellerService()); // Injetamos o service
+//
+//			// O proprio objeto se inscreve para receber o evento
+//			controller.subscribeDataChanceLinstener(this);
+//
+//			controller.updateFormData(); // Vai carregar os dados do Seller vazio nas caixas de texto txtId e
+//											// txtName
+//
+//			// Um palco na frente de outro
+//			Stage dialogStage = new Stage(); // Novo Stage
+//			dialogStage.setTitle("Enter Seller data");
+//			dialogStage.setScene(new Scene(pane)); // Nova scene
+//			dialogStage.setResizable(false); // Se pode ser redimensionada
+//			dialogStage.initOwner(parentStage); // Stage **pai** dessa janela
+//
+//			// Enquanto a janela nao for fechada, nao pode acessar a janela anterior
+//			dialogStage.initModality(Modality.WINDOW_MODAL);
+//			dialogStage.showAndWait();
+//		} catch (IOException e) {
+//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+//		}
+	}
+
+	@Override
+	public void onDataChanged() { 
+		updateTableView();
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) { 
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction( // Vai preencher os dados do departamento a ser atualizado com o *obj*
+						event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
+
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("remove");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+
+	private void removeEntity(Seller obj) {
+		// Resultado do showConfirmation eh um butao clicado
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+		
+		// Optional carrega outro obj dentro dele, podendo estar presente ou nao (para saber usamos o *.get()*)
+		if(result.get() == ButtonType.OK) {
+			if(service == null) {
+				throw new IllegalStateException("Service was null");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();
+			}
+			catch (DbIntegrityException e) {
+				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
+	}
+}
